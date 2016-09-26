d19572321d6696f56be12b9c67db1e416c0ffa20
temocpalma <temopalmag@gmail.com>
2016-09-26 17:35:55 -0500

    #4 Agregando inicialización de super usuario

--- grails-app/init/BootStrap.groovy
+++ grails-app/init/BootStrap.groovy
@@ -1,7 +1,26 @@
+import com.baseapp.Role
+import com.baseapp.User
+import com.baseapp.UserRole
+
 class BootStrap {
 
-    def init = { servletContext ->
+    def init = {
+      def superRole = new Role(authority: 'SUPER_USER').save()
+
+      def superUser = new User(username: 'superUser', password: 'superUser').save()
+
+      UserRole.create superUser, superRole
+
+      UserRole.withSession {
+        it.flush()
+        it.clear()
+      }
+
+      assert User.count() == 1
+      assert Role.count() == 1
+      assert UserRole.count() == 1
     }
+
     def destroy = {
     }
 }
