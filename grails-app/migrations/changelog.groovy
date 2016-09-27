267cdd9194a9d8d67d6e7189976689d5015fd2c2
temocpalma <temopalmag@gmail.com>
2016-09-26 22:39:00 -0500

    #5 Agregando archivo de changelog generado y sincronizado

---
+++ grails-app/migrations/changelog.groovy
@@ -0,0 +1,92 @@
+databaseChangeLog = {
+
+    changeSet(author: "tim (generated)", id: "1474947088936-1") {
+        createTable(tableName: "role") {
+            column(autoIncrement: "true", name: "id", type: "BIGINT") {
+                constraints(primaryKey: "true")
+            }
+
+            column(name: "version", type: "BIGINT") {
+                constraints(nullable: "false")
+            }
+
+            column(name: "authority", type: "VARCHAR(255)") {
+                constraints(nullable: "false")
+            }
+        }
+    }
+
+    changeSet(author: "tim (generated)", id: "1474947088936-2") {
+        createTable(tableName: "user") {
+            column(autoIncrement: "true", name: "id", type: "BIGINT") {
+                constraints(primaryKey: "true")
+            }
+
+            column(name: "version", type: "BIGINT") {
+                constraints(nullable: "false")
+            }
+
+            column(name: "account_expired", type: "BIT(1)") {
+                constraints(nullable: "false")
+            }
+
+            column(name: "account_locked", type: "BIT(1)") {
+                constraints(nullable: "false")
+            }
+
+            column(name: "enabled", type: "BIT(1)") {
+                constraints(nullable: "false")
+            }
+
+            column(name: "password", type: "VARCHAR(255)") {
+                constraints(nullable: "false")
+            }
+
+            column(name: "password_expired", type: "BIT(1)") {
+                constraints(nullable: "false")
+            }
+
+            column(name: "username", type: "VARCHAR(255)") {
+                constraints(nullable: "false")
+            }
+        }
+    }
+
+    changeSet(author: "tim (generated)", id: "1474947088936-3") {
+        createTable(tableName: "user_role") {
+            column(name: "user_id", type: "BIGINT") {
+                constraints(nullable: "false")
+            }
+
+            column(name: "role_id", type: "BIGINT") {
+                constraints(nullable: "false")
+            }
+        }
+    }
+
+    changeSet(author: "tim (generated)", id: "1474947088936-4") {
+        addPrimaryKey(columnNames: "user_id, role_id", constraintName: "PRIMARY", tableName: "user_role")
+    }
+
+    changeSet(author: "tim (generated)", id: "1474947088936-5") {
+        addUniqueConstraint(columnNames: "authority", constraintName: "UK_irsamgnera6angm0prq1kemt2", tableName: "role")
+    }
+
+    changeSet(author: "tim (generated)", id: "1474947088936-6") {
+        addUniqueConstraint(columnNames: "username", constraintName: "UK_sb8bbouer5wak8vyiiy4pf2bx", tableName: "user")
+    }
+
+    changeSet(author: "tim (generated)", id: "1474947088936-7") {
+        createIndex(indexName: "FK_it77eq964jhfqtu54081ebtio", tableName: "user_role") {
+            column(name: "role_id")
+        }
+    }
+
+    changeSet(author: "tim (generated)", id: "1474947088936-8") {
+        addForeignKeyConstraint(baseColumnNames: "user_id", baseTableName: "user_role", constraintName: "FK_apcc8lxk2xnug8377fatvbn04", deferrable: "false", initiallyDeferred: "false", onDelete: "NO ACTION", onUpdate: "NO ACTION", referencedColumnNames: "id", referencedTableName: "user")
+    }
+
+    changeSet(author: "tim (generated)", id: "1474947088936-9") {
+        addForeignKeyConstraint(baseColumnNames: "role_id", baseTableName: "user_role", constraintName: "FK_it77eq964jhfqtu54081ebtio", deferrable: "false", initiallyDeferred: "false", onDelete: "NO ACTION", onUpdate: "NO ACTION", referencedColumnNames: "id", referencedTableName: "role")
+    }
+}
