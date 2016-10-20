98506517b743c86886451879d78cff9d45f4808d
temocpalma <temopalmag@gmail.com>
2016-10-19 21:38:13 -0500

    #6 Modificando el tipo de seguridad a InterceptUrl y agregando el mapeo de permisos

--- grails-app/conf/application.groovy
+++ grails-app/conf/application.groovy
@@ -8,25 +8,23 @@ grails.plugin.springsecurity.authority.className = 'com.baseapp.Role'
 grails.plugin.springsecurity.logout.postOnly = false
 grails.plugin.springsecurity.password.algorithm = 'SHA-256'
 
-grails.plugin.springsecurity.controllerAnnotations.staticRules = [
-	[pattern: '/',               access: ['permitAll']],
-	[pattern: '/error',          access: ['permitAll']],
-	[pattern: '/index',          access: ['permitAll']],
-	[pattern: '/index.gsp',      access: ['permitAll']],
-	[pattern: '/shutdown',       access: ['permitAll']],
-	[pattern: '/assets/**',      access: ['permitAll']],
-	[pattern: '/**/js/**',       access: ['permitAll']],
-	[pattern: '/**/css/**',      access: ['permitAll']],
-	[pattern: '/**/images/**',   access: ['permitAll']],
-	[pattern: '/**/favicon.ico', access: ['permitAll']]
+grails.plugin.springsecurity.securityConfigType = "InterceptUrlMap"
+
+grails.plugin.springsecurity.interceptUrlMap = [
+[pattern: '/',                access: ['permitAll']],
+[pattern: '/error',           access: ['permitAll']],
+[pattern: '/index',           access: ['permitAll']],
+[pattern: '/index.gsp',       access: ['permitAll']],
+[pattern: '/shutdown',        access: ['permitAll']],
+[pattern: '/assets/**',       access: ['permitAll']],
+[pattern: '/**/js/**',        access: ['permitAll']],
+[pattern: '/**/css/**',       access: ['permitAll']],
+[pattern: '/**/images/**',    access: ['permitAll']],
+[pattern: '/**/favicon.ico',  access: ['permitAll']],
+[pattern: '/login',           access: ['permitAll']],
+[pattern: '/login/**',        access: ['permitAll']],
+[pattern: '/logout',          access: ['IS_AUTHENTICATED_FULLY']],
+[pattern: '/logout/**',       access: ['IS_AUTHENTICATED_FULLY']]
 ]
 
-grails.plugin.springsecurity.filterChain.chainMap = [
-	[pattern: '/assets/**',      filters: 'none'],
-	[pattern: '/**/js/**',       filters: 'none'],
-	[pattern: '/**/css/**',      filters: 'none'],
-	[pattern: '/**/images/**',   filters: 'none'],
-	[pattern: '/**/favicon.ico', filters: 'none'],
-	[pattern: '/**',             filters: 'JOINED_FILTERS']
-]
 
