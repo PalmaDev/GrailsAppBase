d4cd544987b4385131ec905d688184aa3c841294
temocpalma <temopalmag@gmail.com>
2017-01-25 23:24:29 -0600

    #11 Agregando configuración spring de database migration y configuración de encode de vistas

--- grails-app/conf/application.groovy
+++ grails-app/conf/application.groovy
@@ -1,4 +1,8 @@
-
+grails.views.default.codec = "html"
+grails.views.gsp.encoding = "UTF-8"
+grails.converters.encoding = "UTF-8"
+grails.plugin.databasemigration.updateOnStart = true
+grails.plugin.databasemigration.updateOnStartFileNames = ['changelog.groovy']
 
 // Added by the Spring Security Core plugin:
 grails.plugin.springsecurity.userLookup.userDomainClassName = 'com.baseapp.User'
