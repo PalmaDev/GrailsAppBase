07d73b79313ae93bb28f11e8e758292517052e7a
temocpalma <temopalmag@gmail.com>
2017-04-20 21:29:49 -0500

    #17 Creando el controller de user con los métodos para crear, guardar y listar usuarios

---
+++ grails-app/controllers/com/baseapp/UserController.groovy
@@ -0,0 +1,33 @@
+package com.baseapp
+
+import grails.transaction.Transactional
+
+@Transactional(readOnly = true)
+class UserController {
+
+  static allowMethods = [saveWithRole:"POST"]
+
+  def userService
+
+  def createUserAdmin(){
+    render view:"createWithRole", model:[roles:[Role.findByAuthority('ADMIN')]]
+  }
+
+  @Transactional
+  def saveWithRole(UserCommand userCommand) {
+    if (userCommand.hasErrors()) {
+      transactionStatus.setRollbackOnly()
+      render view:"createWithRole", model:[user:userCommand]
+    }
+
+    User user = userCommand.createUser()
+    user.save()
+    userService.assignRoleToUser(user, Role.get(userCommand.roleId))
+    redirect action:"list"
+  }
+
+  def list() {
+    params.max = 3
+    [users:User.list(params), usersCount:User.count()]
+  }
+}
