eb5fda19e4376bf4535e1605ca1fd94c4a903107
temocpalma <temopalmag@gmail.com>
2017-06-30 00:58:11 -0500

    #19 Eliminando métodos no necesarios

--- grails-app/controllers/com/baseapp/UserController.groovy
+++ grails-app/controllers/com/baseapp/UserController.groovy
@@ -5,29 +5,10 @@ import grails.transaction.Transactional
 @Transactional(readOnly = true)
 class UserController {
 
-  static allowMethods = [saveWithRole:"POST"]
-
   def userService
 
-  def createUserAdmin(){
-    render view:"createWithRole", model:[roles:[Role.findByAuthority('ADMIN')]]
-  }
-
-  @Transactional
-  def saveWithRole(UserWithRoleCommand userCommand) {
-    if (userCommand.hasErrors()) {
-      transactionStatus.setRollbackOnly()
-      render view:"createWithRole", model:[user:userCommand]
-    }
-
-    User user = userCommand.createUser()
-    user.save()
-    userService.assignRoleToUser(user, Role.get(userCommand.roleId))
-    redirect action:"list"
-  }
-
   def list() {
-    params.max = 20
-    [users:User.list(params), usersCount:User.count()]
+    [usersList:userService.getListUsers(params), usersCount:User.count()]
   }
+
 }
