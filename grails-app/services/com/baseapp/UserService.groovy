f8ef6db2c465bf631df939d56f1467a5869ea8d8
temocpalma <temopalmag@gmail.com>
2017-06-29 23:41:21 -0500

    #19 Creando método para guardar un usuario, y otro para obtener la lista de usuarios paginada

--- grails-app/services/com/baseapp/UserService.groovy
+++ grails-app/services/com/baseapp/UserService.groovy
@@ -1,8 +1,20 @@
 package com.baseapp
 
+import grails.transaction.Transactional
+
 class UserService {
 
   void assignRoleToUser(User user, Role role) {
     UserRole.create user, role
   }
+
+  @Transactional
+  User saveUser(User user) {
+    user.save()
+  }
+
+  List<User> getListUsers(Map params) {
+    User.list(max:params.max ?: 20, offset:params.offset ?: 0)
+  }
+
 }
